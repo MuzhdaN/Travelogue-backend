057abde75242e9f7f317d1f8d7856440cd6db0ce
MuzhdaN <muzhda.noorzad4@gmail.com>
2023-03-23 03:14:59 +0000

    Added profiles detail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,21 +5,20 @@ from .models import Profile
 # convert python models to json
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
+    # is_owner = serializers.SerializerMethodField()
 
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
+    # def get_is_owner(self, obj):
+    #     request = self.context['request']
+    #     return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id',
-            'owner', 
-            'created_at', 
-            'updated_at', 
-            'name', 
-            'content', 
+            'owner',
+            'created_at',
+            'updated_at',
+            'name',
+            'content',
             'image',
-            'is_owner',
         ]
