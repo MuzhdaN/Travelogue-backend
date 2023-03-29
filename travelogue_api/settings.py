219e7caa8618792a925db58032f095dab572d7f8
MuzhdaN <muzhda.noorzad4@gmail.com>
2023-03-29 08:52:29 +0000

    Fixed typo for deployement

--- travelogue_api/settings.py
+++ travelogue_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://git.heroku.com/travelogue-api.git']
+ALLOWED_HOST = ['localhost', 'https://git.heroku.com/travelogue-api.git']
 
 
 # Application definition
